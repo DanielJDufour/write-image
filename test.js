5ddd95f4fa89afd9b4bf3cdc1a3406cb3188808e
DanielJDufour <daniel.j.dufour@gmail.com>
2021-10-30 23:04:48 -0400

    fixed bug where height and width where undefined

--- test.js
+++ test.js
@@ -25,3 +25,46 @@ test("png to jpg", async ({ eq }) => {
   eq(result.width, width);
   eq(Buffer.isBuffer(result.data), true);
 });
+
+test("png to png", async ({ eq }) => {
+  const png = findAndRead("flower.png");
+  const { height, width, pixels } = await readim({ data: png });
+  const result = writeImage({ data: pixels, debug: false, format: "png", height, width });
+  fs.writeFileSync("png-to-png.png", result.data);
+  eq(result.height, height);
+  eq(result.width, width);
+  eq(Buffer.isBuffer(result.data), true);
+});
+
+test("jpg to jpg", async ({ eq }) => {
+  const jpg = findAndRead("flower.jpg");
+  const { height, width, pixels } = await readim({ data: jpg });
+  const result = writeImage({ data: pixels, debug: false, format: "jpg", height, width });
+  fs.writeFileSync("jpg-to-jpg.jpg", result.data);
+  eq(result.height, height);
+  eq(result.width, width);
+  eq(Buffer.isBuffer(result.data), true);
+});
+
+test("unknown size", async ({ eq }) => {
+  const png = findAndRead("flower-big.jpg");
+  const { height, width, pixels } = await readim({ data: png });
+
+  // transform pixels from rgab to [band][row][column]
+  const { data } = xdim.transform({
+    data: pixels,
+    from: "[row,column][band]",
+    to: "[band][row][column]",
+    sizes: {
+      band: 4,
+      column: 460,
+      row: 460
+    }
+  });
+
+  const result = writeImage({ data, debug: false, format: "png", height: undefined, width: undefined });
+  fs.writeFileSync("unknown-size.png", result.data);
+  eq(result.height, height);
+  eq(result.width, width);
+  eq(Buffer.isBuffer(result.data), true);
+});
--- write-image.js
+++ write-image.js
@@ -12,12 +12,16 @@ module.exports = function writeImage({ data, debug = false, format, height, widt
   const imageData = toImageData({ data, height, width });
   if (debug) console.log(`[write-image] image data is`, imageData);
 
+  if (height === undefined || height === null) height = imageData.height;
+  if (width === undefined || width === null) width = imageData.width;
+
   let result;
   if (format === "PNG") {
     const png = new PNG({ filterType: 4, height, width });
     png.data = imageData.data;
     if (debug) console.log(`[write-image] png is`, png);
     const buffer = PNG.sync.write(png);
+    if (debug) console.log(`[write-image] wrote buffer of ${buffer.byteLength} bytes`);
     result = { data: buffer, height, width };
   } else if (format === "JPG") {
     const encoded = jpeg.encode(imageData, quality).data;
